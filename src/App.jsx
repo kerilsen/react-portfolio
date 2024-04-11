17189806b4197df4a9bcd426e95c1f41736f274a
Keri Sen <147566619+kerilsen@users.noreply.github.com>
2024-04-11 11:21:57 -0700

    Header and footer components are working without navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Page from "./pages/Page";
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import './assets/reset.css'
+import './assets/mystyle.css'
+// import { useLocation } from "react-router-dom";
+
 export default function App() {
-  return Hello
+  // const currentPage = useLocation().pathname;
+
+  return (
+    <>
+      <Header>
+        {/* <Nav currentPage={currentPage} /> */}
+      </Header>
+      <main>
+        {/* <Portfolio /> */}
+        {/* <Page currentPage={currentPage} /> */}
+      </main>
+      <Footer />
+    </>
+  );
 }
